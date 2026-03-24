32d16602715bd59b4bcfc359251d0d920a67f190
drawcodeboy <20rnjsekdns@naver.com>
2026-03-24 21:51:04 +0900

    [Fix] line by line

--- scripts/train_convnet_mnist_cls.sh
+++ scripts/train_convnet_mnist_cls.sh
@@ -14,11 +14,10 @@ ERR=$LOG_DIR/$(date +%Y%m%d-%H%M%S).err
 exec 1> "$OUT"
 exec 2> "$ERR"
 
-export PYTHON_UNBUFFERED=1
-
 # Options:
 #   export WANDB_API_KEY=your_wandb_api_key_here
 #   --use_wandb (log to wandb)
 #   --resume (load last.ckpt)
 
+export PYTHON_UNBUFFERED=1
 python -u train.py --config=convnet_mnist_cls --resume
